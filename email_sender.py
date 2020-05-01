a94cb22585de129a39b8240ac44ff7dcffe32be3
firdavsxon <akilovfs@gmail.com>
2020-05-01 11:44:38 -0700

    change

--- email_sender.py
+++ email_sender.py
@@ -7,14 +7,14 @@ html = Template(Path('index.html').read_text())
 
 email = EmailMessage()
 email['from'] = 'First_name Last_name'
-email['to'] = 'akilovfs@gmail.com'
+email['to'] = 'example@gmail.com'
 email['subject'] = 'Adding new car message!'
 email.set_content(html.substitute({'name':'TinTin'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
 	smtp.ehlo()
 	smtp.starttls()
-	smtp.login('dummy.bummy777@gmail.com', 'samar_uzb_1234')
+	smtp.login('example@gmail.com', 'password')
 	for _ in range(3):
 		smtp.send_message(email)
 	print('All good boss!')
