decbe260a00c0d1cc35531c29501389511f74c76
firdavsxon <akilovfs@gmail.com>
2020-04-30 19:17:53 -0700

    update

--- email_sender.py
+++ email_sender.py
@@ -2,14 +2,15 @@ import smtplib
 from email.message import EmailMessage
 
 email = EmailMessage()
-email['from'] = 'Firdavs Akilov'
-email['to'] = 'akilovfs@gmail.com'
-email['subject'] = 'You won 1 000 000 dollars!'
+email['from'] = 'Python Test'
+email['to'] = 'name@gmail.com'
+email['subject'] = 'python message!'
 email.set_content('I am a python master')
 
-with smtplib.SMTP(host='gmail.com', port=587) as smtp:
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
 	smtp.ehlo()
 	smtp.starttls()
-	smtp.login('dummy.bummy777@gmail.com', 'apartment104')
-	smtp.send_message(email)
+	smtp.login('name@gmail.com', 'password')
+	for _ in range(3):
+		smtp.send_message(email)
 	print('All good boss!')
