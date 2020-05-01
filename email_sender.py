034c6b58c515b117ddfc7e9dc28539ba7a4fd1fc
firdavsxon <akilovfs@gmail.com>
2020-05-01 11:40:18 -0700

    adding another froms

--- email_sender.py
+++ email_sender.py
@@ -1,16 +1,20 @@
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
 
 email = EmailMessage()
-email['from'] = 'Python Test'
-email['to'] = 'name@gmail.com'
-email['subject'] = 'python message!'
-email.set_content('I am a python master')
+email['from'] = 'First_name Last_name'
+email['to'] = 'akilovfs@gmail.com'
+email['subject'] = 'Adding new car message!'
+email.set_content(html.substitute({'name':'TinTin'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
 	smtp.ehlo()
 	smtp.starttls()
-	smtp.login('name@gmail.com', 'password')
+	smtp.login('dummy.bummy777@gmail.com', 'samar_uzb_1234')
 	for _ in range(3):
 		smtp.send_message(email)
 	print('All good boss!')
